946d19f693c6927b0cc3869e9e114c5ceba8e821
Jaroslav Tyc <email@jaroslavtyc.com>
2015-08-04 06:43:51 +0200

    fix: Size has property interface

--- DrdPlus/Properties/Body/Size.php
+++ DrdPlus/Properties/Body/Size.php
@@ -1,20 +1,22 @@
 <?php
 namespace DrdPlus\Properties\Body;
 
+use DrdPlus\Properties\PropertyInterface;
 use Granam\Integer\IntegerObject;
 
 /**
  * note: Size does not need to be persisted and therefore does not need enum type.
  */
-class Size extends IntegerObject
+class Size extends IntegerObject implements PropertyInterface
 {
     const SIZE = 'size';
 
     /**
      * @return string
      */
-    public function getName()
+    public function getCode()
     {
         return self::SIZE;
     }
+
 }
