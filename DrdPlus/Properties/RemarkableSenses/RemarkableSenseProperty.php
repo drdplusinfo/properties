70483aa3d195e88440508f8eceea3fe9e6f3c79f
Jaroslav Tyc <email@jaroslavtyc.com>
2015-11-11 17:35:15 +0100

    Removed duplicated interface usage

--- DrdPlus/Properties/RemarkableSenses/RemarkableSenseProperty.php
+++ DrdPlus/Properties/RemarkableSenses/RemarkableSenseProperty.php
@@ -2,12 +2,11 @@
 namespace DrdPlus\Properties\RemarkableSenses;
 
 use DrdPlus\Properties\AbstractStringProperty;
-use DrdPlus\Properties\PropertyInterface;
 
 /**
  * @method static RemarkableSenseProperty getIt()
  */
-abstract class RemarkableSenseProperty extends AbstractStringProperty implements PropertyInterface
+abstract class RemarkableSenseProperty extends AbstractStringProperty
 {
 
 }
