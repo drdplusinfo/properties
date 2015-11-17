26668ffe46e18ee5f551f28b96ce9601bf99e492
jaroslavtyc <mail@jaroslavtyc.com>
2015-11-17 09:06:59 +0100

    Senses creation simplified

--- DrdPlus/Properties/Derived/Senses.php
+++ DrdPlus/Properties/Derived/Senses.php
@@ -4,15 +4,15 @@ namespace DrdPlus\Properties\Derived;
 use DrdPlus\Codes\PropertyCodes;
 use DrdPlus\Properties\Base\Knack;
 use DrdPlus\Properties\Derived\Parts\AbstractDerivedProperty;
-use Granam\Integer\IntegerInterface;
+use Granam\Integer\Tools\ToInteger;
 
 class Senses extends AbstractDerivedProperty
 {
     const SENSES = PropertyCodes::SENSES;
 
-    public function __construct(Knack $knack, IntegerInterface $raceGenderSenses)
+    public function __construct(Knack $knack, $raceGenderSenses)
     {
-        $this->value = $knack->getValue() + $raceGenderSenses->getValue();
+        $this->value = $knack->getValue() + ToInteger::toInteger($raceGenderSenses);
     }
 
     public function getCode()
--- DrdPlus/Tests/Properties/Derived/SensesTest.php
+++ DrdPlus/Tests/Properties/Derived/SensesTest.php
@@ -3,7 +3,6 @@ namespace DrdPlus\Tests\Properties\Derived;
 
 use DrdPlus\Properties\Base\Knack;
 use DrdPlus\Properties\Derived\Senses;
-use Granam\Integer\IntegerObject;
 
 class SensesTest extends AbstractTestOfDerivedProperty
 {
@@ -12,7 +11,7 @@ class SensesTest extends AbstractTestOfDerivedProperty
      */
     public function I_can_get_property_easily()
     {
-        $senses = new Senses($this->getKnack($knackValue = 123), new IntegerObject($raceGenderSenses = 456));
+        $senses = new Senses($this->getKnack($knackValue = 123), $raceGenderSenses = 456);
         $this->assertSame('senses', $senses->getCode());
         $this->assertSame('senses', $senses::SENSES);
         $this->assertSame($knackValue + $raceGenderSenses, $senses->getValue());
