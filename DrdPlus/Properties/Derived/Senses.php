6bfc47bf20953d3ee7ca5893e6625847418b3a2a
jaroslavtyc <mail@jaroslavtyc.com>
2016-08-15 10:47:16 +0200

    Senses require cornerstone-like parameters instead of final race sense

--- DrdPlus/Properties/Derived/Senses.php
+++ DrdPlus/Properties/Derived/Senses.php
@@ -2,17 +2,26 @@
 namespace DrdPlus\Properties\Derived;
 
 use DrdPlus\Codes\PropertyCode;
+use DrdPlus\Codes\RaceCode;
+use DrdPlus\Codes\SubRaceCode;
 use DrdPlus\Properties\Base\Knack;
 use DrdPlus\Properties\Derived\Parts\AbstractDerivedProperty;
+use DrdPlus\Tables\Races\RacesTable;
 use Granam\Integer\Tools\ToInteger;
 
 class Senses extends AbstractDerivedProperty
 {
     const SENSES = PropertyCode::SENSES;
 
-    public function __construct(Knack $knack, $raceGenderSenses)
+    public function __construct(
+        Knack $knack,
+        RaceCode $raceCode,
+        SubRaceCode $subRaceCode,
+        RacesTable $racesTable
+    )
     {
-        $this->value = $knack->getValue() + ToInteger::toInteger($raceGenderSenses);
+        $this->value = $knack->getValue()
+            + ToInteger::toInteger($racesTable->getSenses($raceCode->getValue(), $subRaceCode->getValue()));
     }
 
     public function getCode()
--- DrdPlus/Tests/Properties/Derived/SensesTest.php
+++ DrdPlus/Tests/Properties/Derived/SensesTest.php
@@ -1,18 +1,27 @@
 <?php
 namespace DrdPlus\Tests\Properties\Derived;
 
+use DrdPlus\Codes\RaceCode;
+use DrdPlus\Codes\SubRaceCode;
 use DrdPlus\Properties\Base\Knack;
 use DrdPlus\Properties\Derived\Senses;
+use DrdPlus\Tables\Races\RacesTable;
 use DrdPlus\Tests\Properties\Derived\Parts\AbstractDerivedPropertyTest;
 
 class SensesTest extends AbstractDerivedPropertyTest
 {
     /**
      * #@test
+     * @return Senses
      */
     public function I_can_get_property_easily()
     {
-        $senses = new Senses($this->getKnack($knackValue = 123), $raceGenderSenses = 456);
+        $senses = new Senses(
+            $this->createKnack($knackValue = 123),
+            $this->createRaceCode('foo'),
+            $this->createSubRaceCode('bar'),
+            $this->createRacesTable('foo', 'bar', $raceGenderSenses = 456)
+        );
         self::assertSame('senses', $senses->getCode());
         self::assertSame('senses', Senses::SENSES);
         self::assertSame($knackValue + $raceGenderSenses, $senses->getValue());
@@ -25,8 +34,50 @@ class SensesTest extends AbstractDerivedPropertyTest
      * @param $value
      * @return \Mockery\MockInterface|Knack
      */
-    private function getKnack($value)
+    private function createKnack($value)
     {
         return $this->createProperty(Knack::class, $value);
+    }
+
+    /**
+     * @param $value
+     * @return \Mockery\MockInterface|RaceCode
+     */
+    private function createRaceCode($value)
+    {
+        $raceCode = $this->mockery(RaceCode::class);
+        $raceCode->shouldReceive('getValue')
+            ->andReturn($value);
+
+        return $raceCode;
+    }
+
+    /**
+     * @param $value
+     * @return \Mockery\MockInterface|SubRaceCode
+     */
+    private function createSubRaceCode($value)
+    {
+        $subRaceCode = $this->mockery(SubRaceCode::class);
+        $subRaceCode->shouldReceive('getValue')
+            ->andReturn($value);
+
+        return $subRaceCode;
+    }
+
+    /**
+     * @param $expectedRacesCode
+     * @param $expectedSubRaceCode
+     * @param $senses
+     * @return \Mockery\MockInterface|RacesTable
+     */
+    private function createRacesTable($expectedRacesCode, $expectedSubRaceCode, $senses)
+    {
+        $racesTable = $this->mockery(RacesTable::class);
+        $racesTable->shouldReceive('getSenses')
+            ->with($expectedRacesCode, $expectedSubRaceCode)
+            ->andReturn($senses);
+
+        return $racesTable;
     }
 }
