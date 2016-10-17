56581092e91ff37dd3f319d2895cd2eacb426282
Jaroslav Týc <mail@jaroslavtyc.com>
2016-10-17 18:41:00 +0200

    Encounter range can be converted to meters

--- DrdPlus/Properties/Combat/EncounterRange.php
+++ DrdPlus/Properties/Combat/EncounterRange.php
@@ -2,8 +2,19 @@
 namespace DrdPlus\Properties\Combat;
 
 use DrdPlus\Properties\Combat\Partials\PositiveNumberCombatGameCharacteristics;
+use DrdPlus\Tables\Measurements\Distance\DistanceBonus;
+use DrdPlus\Tables\Measurements\Distance\DistanceTable;
 
 class EncounterRange extends PositiveNumberCombatGameCharacteristics
 {
-
+    /**
+     * @param DistanceTable $distanceTable
+     * @return int
+     */
+    public function getInMeters(DistanceTable $distanceTable)
+    {
+        // encounter range is in fact distance bonus
+        /** @noinspection ExceptionsAnnotatingAndHandlingInspection */
+        return (new DistanceBonus($this->getValue(), $distanceTable))->getDistance()->getMeters();
+    }
 }
--- DrdPlus/Tests/Properties/Combat/EncounterRangeTest.php
+++ DrdPlus/Tests/Properties/Combat/EncounterRangeTest.php
@@ -2,6 +2,9 @@
 namespace DrdPlus\Tests\Properties\Combat;
 
 use DrdPlus\Properties\Combat\EncounterRange;
+use DrdPlus\Tables\Measurements\Distance\Distance;
+use DrdPlus\Tables\Measurements\Distance\DistanceBonus;
+use DrdPlus\Tables\Measurements\Distance\DistanceTable;
 use DrdPlus\Tests\Properties\Combat\Partials\PositiveNumberCombatGameCharacteristicsTest;
 
 class EncounterRangeTest extends PositiveNumberCombatGameCharacteristicsTest
@@ -10,6 +13,28 @@ class EncounterRangeTest extends PositiveNumberCombatGameCharacteristicsTest
     protected function createSut()
     {
         return new EncounterRange(123);
+    }
+
+    /**
+     * @test
+     */
+    public function I_can_get_its_value_in_meters()
+    {
+        $encounterRange = new EncounterRange(123);
+        $distanceTable = $this->mockery(DistanceTable::class);
+        $distanceValue = 456;
+        $distanceTable->shouldReceive('toDistance')
+            ->with(\Mockery::type(DistanceBonus::class))
+            ->andReturnUsing(function (DistanceBonus $distanceBonus) use ($distanceValue) {
+                self::assertSame(123, $distanceBonus->getValue());
+                $distance = $this->mockery(Distance::class);
+                $distance->shouldReceive('getMeters')
+                    ->andReturn($distanceValue);
+
+                return $distance;
+            });
+
+        self::assertSame($distanceValue, $encounterRange->getInMeters($distanceTable));
     }
 
 }
