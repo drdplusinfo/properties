3b5eb34f095fbf30dcd02e30d125dd96b1164483
Jaroslav Týc <mail@jaroslavtyc.com>
2016-11-13 13:36:51 +0100

    Integer property tracks its history of changes

--- DrdPlus/Properties/AbstractIntegerProperty.php
+++ DrdPlus/Properties/AbstractIntegerProperty.php
@@ -2,12 +2,18 @@
 namespace DrdPlus\Properties;
 
 use Doctrineum\Integer\IntegerEnum;
+use Granam\Integer\IntegerInterface;
 use Granam\Integer\Tools\ToInteger;
+use Granam\Tools\ValueDescriber;
 
 abstract class AbstractIntegerProperty extends IntegerEnum implements Property
 {
 
+    protected $history = [];
+
     /**
+     * Will give clone.
+     *
      * @param int $value
      * @return IntegerEnum|AbstractIntegerProperty
      */
@@ -17,24 +23,138 @@ abstract class AbstractIntegerProperty extends IntegerEnum implements Property
     }
 
     /**
-     * @param int|static $value
-     * @return static
+     * Will give clone.
+     *
+     * @param int $enumValue
+     * @return IntegerEnum|AbstractIntegerProperty
+     */
+    public static function getEnum($enumValue)
+    {
+        /** @var AbstractIntegerProperty $integerProperty */
+        $integerProperty = clone parent::getEnum($enumValue); // cloned to get unique history
+        $integerProperty->noticeChange();
+
+        return $integerProperty;
+    }
+
+    public function __clone()
+    {
+        // overloaded parent to allow cloning (to get clean property WITHOUT history)
+    }
+
+    private function noticeChange()
+    {
+        $backtrace = debug_backtrace();
+        $changingCall = $this->findChangingCall($backtrace); // penultimate step (that before calling noticeChange)
+        $this->history[] = [
+            'changeBy' => [
+                'name' => $this->formatToSentence($changingCall['function']),
+                'arguments' => $this->extractArgumentsDescription($changingCall['args']),
+            ],
+            'result' => $this->getValue(),
+        ];
+    }
+
+    /**
+     * @param array $backtrace
+     * @return array
+     */
+    private function findChangingCall(array $backtrace)
+    {
+        /** @var array $call */
+        foreach ($backtrace as $call) {
+            if (array_key_exists('class', $call) && $call['class'] === __CLASS__) {
+                continue;
+            }
+            if (array_key_exists('object', $call) && $call['object'] === $this) {
+                continue;
+            }
+
+            return $call;
+        }
+
+        return ['function' => '?', 'args' => []];
+    }
+
+    /**
+     * @param string $string
+     * @return string
+     */
+    private function formatToSentence($string)
+    {
+        preg_match_all('~[[:upper:]]?[[:lower:]]*~', $string, $matches);
+        $captures = array_filter($matches[0], function ($capture) {
+            return $capture !== '';
+        });
+
+        return implode(
+            ' ',
+            array_map(
+                function ($name) {
+                    return lcfirst($name);
+                },
+                $captures
+            )
+        );
+    }
+
+    /**
+     * @param array $arguments
+     * @return string
+     */
+    private function extractArgumentsDescription(array $arguments)
+    {
+        $descriptions = [];
+        foreach ($arguments as $argument) {
+            $descriptions[] = ValueDescriber::describe($argument);
+        }
+
+        return implode(',', $descriptions);
+    }
+
+    /**
+     * @param int|IntegerInterface $value
+     * @return AbstractIntegerProperty
      * @throws \Granam\Integer\Tools\Exceptions\WrongParameterType
      * @throws \Granam\Integer\Tools\Exceptions\ValueLostOnCast
      */
     public function add($value)
     {
-        return static::getIt($this->getValue() + ToInteger::toInteger($value));
+        $increased = static::getIt($this->getValue() + ToInteger::toInteger($value));
+        $increased->adoptHistory($this);
+
+        return $increased;
+    }
+
+    private function adoptHistory(AbstractIntegerProperty $integerProperty)
+    {
+        // previous history FIRST, current after
+        $this->history = array_merge($integerProperty->getHistory(), $this->getHistory());
     }
 
     /**
-     * @param int|static $value
-     * @return static
+     * @param int|IntegerInterface $value
+     * @return AbstractIntegerProperty
      * @throws \Granam\Integer\Tools\Exceptions\WrongParameterType
      * @throws \Granam\Integer\Tools\Exceptions\ValueLostOnCast
      */
     public function sub($value)
     {
-        return static::getIt($this->getValue() - ToInteger::toInteger($value));
+        $decreased = static::getIt($this->getValue() - ToInteger::toInteger($value));
+        $decreased->adoptHistory($this);
+
+        return $decreased;
+    }
+
+    /**
+     * Gives array of modifications and result values, from very first to current value.
+     * Order of historical values is from oldest as first to newest as last.
+     * Warning: history is NOT persisted.
+     *
+     * @return array
+     */
+    public function getHistory()
+    {
+        return $this->history;
     }
 }
--- DrdPlus/Tests/Properties/AbstractIntegerPropertyTest.php
+++ DrdPlus/Tests/Properties/AbstractIntegerPropertyTest.php
@@ -2,6 +2,7 @@
 namespace DrdPlus\Tests\Properties;
 
 use DrdPlus\Properties\AbstractIntegerProperty;
+use Granam\Tools\ValueDescriber;
 
 abstract class AbstractIntegerPropertyTest extends AbstractTestOfStoredProperty
 {
@@ -16,33 +17,112 @@ abstract class AbstractIntegerPropertyTest extends AbstractTestOfStoredProperty
 
     /**
      * @test
+     * @dataProvider provideSomeArguments
+     * @param $justSomeArgument
+     * @param $justAnotherArgument
      */
-    public function I_can_add_value()
+    public function I_can_add_value($justSomeArgument, $justAnotherArgument)
     {
         $propertyClass = $this->getSutClass();
         /** @var AbstractIntegerProperty $property */
         $property = $propertyClass::getIt(123);
+        $propertyHistory = $property->getHistory();
+        $expectedChangeBy = [
+            'name' => 'i can add value',
+            'arguments' => implode(
+                ',',
+                [ValueDescriber::describe($justSomeArgument), ValueDescriber::describe($justAnotherArgument)]
+            ),
+        ];
+        $expectedPropertyHistory = [
+            [
+                'changeBy' => $expectedChangeBy,
+                'result' => $property->getValue(),
+            ],
+        ];
+        self::assertEquals($expectedPropertyHistory, $propertyHistory);
+
         $greater = $property->add(456);
+        $greaterHistory = $greater->getHistory();
+        $expectedGreaterHistoryChange = [
+            'changeBy' => $expectedChangeBy,
+            'result' => $greater->getValue(),
+        ];
+        $expectedGreaterHistory = $expectedPropertyHistory;
+        $expectedGreaterHistory[] = $expectedGreaterHistoryChange;
+        self::assertEquals($expectedGreaterHistory, $greaterHistory);
         self::assertSame(123, $property->getValue());
         self::assertNotEquals($property, $greater);
         self::assertSame(579, $greater->getValue());
+
         $double = $greater->add($greater);
+        $doubleHistory = $double->getHistory();
+        $expectedDoubleHistoryChange = [
+            'changeBy' => $expectedChangeBy,
+            'result' => $double->getValue(),
+        ];
+        $expectedDoubleHistory = $expectedGreaterHistory;
+        $expectedDoubleHistory[] = $expectedDoubleHistoryChange;
+        self::assertEquals($expectedDoubleHistory, $doubleHistory);
         self::assertSame(1158, $double->getValue());
+    }
+
+    public function provideSomeArguments()
+    {
+        return [
+            ['foo', new \DateTime()],
+        ];
     }
 
     /**
      * @test
+     * @dataProvider provideSomeArguments
+     * @param $justSomeArgument
+     * @param $justAnotherArgument
      */
-    public function I_can_subtract_value()
+    public function I_can_subtract_value($justSomeArgument, $justAnotherArgument)
     {
         $propertyClass = $this->getSutClass();
         /** @var AbstractIntegerProperty $property */
         $property = $propertyClass::getIt(123);
-        $low = $property->sub(456);
+        $propertyHistory = $property->getHistory();
+        $expectedChangeBy = [
+            'name' => 'i can subtract value',
+            'arguments' => implode(
+                ',',
+                [ValueDescriber::describe($justSomeArgument), ValueDescriber::describe($justAnotherArgument)]
+            ),
+        ];
+        $expectedPropertyHistory = [
+            [
+                'changeBy' => $expectedChangeBy,
+                'result' => $property->getValue(),
+            ],
+        ];
+        self::assertEquals($expectedPropertyHistory, $propertyHistory);
+
+        $lesser = $property->sub(456);
         self::assertSame(123, $property->getValue());
-        self::assertNotEquals($property, $low);
-        self::assertSame(-333, $low->getValue());
-        $zero = $low->sub($low);
+        self::assertNotEquals($property, $lesser);
+        self::assertSame(-333, $lesser->getValue());
+        $lesserHistory = $lesser->getHistory();
+        $expectedLesserHistoryChange = [
+            'changeBy' => $expectedChangeBy,
+            'result' => $lesser->getValue(),
+        ];
+        $expectedLesserHistory = $expectedPropertyHistory;
+        $expectedLesserHistory[] = $expectedLesserHistoryChange;
+        self::assertEquals($expectedLesserHistory, $lesserHistory);
+
+        $zero = $lesser->sub($lesser);
+        $zeroHistory = $zero->getHistory();
+        $expectedLesserHistoryChange = [
+            'changeBy' => $expectedChangeBy,
+            'result' => $zero->getValue(),
+        ];
+        $expectedZeroHistory = $expectedLesserHistory;
+        $expectedZeroHistory[] = $expectedLesserHistoryChange;
+        self::assertEquals($expectedZeroHistory, $zeroHistory);
         self::assertSame(0, $zero->getValue());
     }
 
