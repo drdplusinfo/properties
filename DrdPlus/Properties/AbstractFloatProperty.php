61bca112a13667af2728f54d8007cfa0dd98dc9e
Jaroslav Týc <mail@jaroslavtyc.com>
2016-11-15 08:38:35 +0100

    Removed unused interface to modify a float property

--- DrdPlus/Properties/AbstractFloatProperty.php
+++ DrdPlus/Properties/AbstractFloatProperty.php
@@ -3,7 +3,6 @@ namespace DrdPlus\Properties;
 
 use Doctrineum\Float\FloatEnum;
 use Granam\Float\FloatInterface;
-use Granam\Float\Tools\ToFloat;
 
 /**
  * @method static AbstractFloatProperty getEnum(float $enumValue)
@@ -18,30 +17,6 @@ abstract class AbstractFloatProperty extends FloatEnum implements Property, Floa
     public static function getIt($value)
     {
         return static::getEnum($value);
-    }
-
-    /**
-     * @param float|static $value
-     * @return AbstractFloatProperty
-     * @throws \Granam\Number\Tools\Exceptions\WrongParameterType
-     * @throws \Granam\Scalar\Tools\Exceptions\WrongParameterType
-     */
-    public function add($value)
-    {
-        /** @noinspection ExceptionsAnnotatingAndHandlingInspection */
-        return static::getIt($this->getValue() + ToFloat::toFloat($value));
-    }
-
-    /**
-     * @param float|static $value
-     * @return AbstractFloatProperty
-     * @throws \Granam\Number\Tools\Exceptions\WrongParameterType
-     * @throws \Granam\Scalar\Tools\Exceptions\WrongParameterType
-     */
-    public function sub($value)
-    {
-        /** @noinspection ExceptionsAnnotatingAndHandlingInspection */
-        return static::getIt($this->getValue() - ToFloat::toFloat($value));
     }
 
 }
--- DrdPlus/Tests/Properties/AbstractFloatPropertyTest.php
+++ DrdPlus/Tests/Properties/AbstractFloatPropertyTest.php
@@ -1,45 +1,11 @@
 <?php
 namespace DrdPlus\Tests\Properties;
 
-use DrdPlus\Properties\AbstractFloatProperty;
-
 abstract class AbstractFloatPropertyTest extends AbstractTestOfStoredProperty
 {
 
     protected function getValuesForTest()
     {
         return [0.01, 123.456];
-    }
-
-    /**
-     * @test
-     */
-    public function I_can_add_value()
-    {
-        $propertyClass = $this->getSutClass();
-        /** @var AbstractFloatProperty $property */
-        $property = $propertyClass::getIt(123.456);
-        $greater = $property->add(456.789);
-        self::assertSame(123.456, $property->getValue());
-        self::assertNotEquals($property, $greater);
-        self::assertSame(580.245, $greater->getValue());
-        $double = $greater->add($greater);
-        self::assertSame(1160.490, $double->getValue());
-    }
-
-    /**
-     * @test
-     */
-    public function I_can_subtract_value()
-    {
-        $propertyClass = $this->getSutClass();
-        /** @var AbstractFloatProperty $property */
-        $property = $propertyClass::getIt(0.5);
-        $low = $property->sub(0.2);
-        self::assertSame(0.5, $property->getValue());
-        self::assertNotEquals($property, $low);
-        self::assertSame(0.3, $low->getValue());
-        $zero = $low->sub($low);
-        self::assertSame(0.0, $zero->getValue());
     }
 }
